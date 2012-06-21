12033ee9fd4ecb9d4cb37bf0b10349c6b13b37a0
Robert Böhnke <robb@robb.is>
2012-06-21 16:21:54 -0700

    Initial Commit

---
+++ UITableView+NXEmptyViewTests/UITableView_NXEmptyViewTests.h
@@ -0,0 +1,13 @@
+//
+//  UITableView_NXEmptyViewTests.h
+//  UITableView+NXEmptyViewTests
+//
+//  Created by Ullrich Schäfer on 21.06.12.
+//  Copyright (c) 2012 nxtbgthng.com. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface UITableView_NXEmptyViewTests : SenTestCase
+
+@end
---
+++ UITableView+NXEmptyViewTests/UITableView_NXEmptyViewTests.m
@@ -0,0 +1,32 @@
+//
+//  UITableView_NXEmptyViewTests.m
+//  UITableView+NXEmptyViewTests
+//
+//  Created by Ullrich Schäfer on 21.06.12.
+//  Copyright (c) 2012 nxtbgthng.com. All rights reserved.
+//
+
+#import "UITableView_NXEmptyViewTests.h"
+
+@implementation UITableView_NXEmptyViewTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in UITableView+NXEmptyViewTests");
+}
+
+@end
