8ec02eb4986de6413a4c8f66ca937344f415f1d1
Orlando Aleman Ortiz <contacto@orlandoaleman.com>
2014-04-08 00:59:23 +0100

    Update to latest OCMock release

--- Libraries/OCMock.framework/Versions/A/Headers/OCMArg.h
+++ Libraries/OCMock.framework/Versions/A/Headers/OCMArg.h
@@ -1,6 +1,6 @@
 //---------------------------------------------------------------------------------------
 //  $Id$
-//  Copyright (c) 2009-2010 by Mulle Kybernetik. See License file for details.
+//  Copyright (c) 2009-2013 by Mulle Kybernetik. See License file for details.
 //---------------------------------------------------------------------------------------
 
 #import <Foundation/Foundation.h>
@@ -10,18 +10,21 @@
 // constraining arguments
 
 + (id)any;
++ (SEL)anySelector;
 + (void *)anyPointer;
++ (id __autoreleasing *)anyObjectRef;
 + (id)isNil;
 + (id)isNotNil;
 + (id)isNotEqual:(id)value;
 + (id)checkWithSelector:(SEL)selector onObject:(id)anObject;
 #if NS_BLOCKS_AVAILABLE
-+ (id)checkWithBlock:(BOOL (^)(id))block;
++ (id)checkWithBlock:(BOOL (^)(id obj))block;
 #endif
 
 // manipulating arguments
 
 + (id *)setTo:(id)value;
++ (void *)setToValue:(NSValue *)value;
 
 // internal use only
 
@@ -30,4 +33,10 @@
 @end
 
 #define OCMOCK_ANY [OCMArg any]
-#define OCMOCK_VALUE(variable) [NSValue value:&variable withObjCType:@encode(__typeof__(variable))]
+
+#if defined(__GNUC__) && !defined(__STRICT_ANSI__)
+  #define OCMOCK_VALUE(variable) \
+    ({ __typeof__(variable) __v = (variable); [NSValue value:&__v withObjCType:@encode(__typeof__(__v))]; })
+#else
+  #define OCMOCK_VALUE(variable) [NSValue value:&variable withObjCType:@encode(__typeof__(variable))]
+#endif
